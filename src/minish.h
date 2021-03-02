fca565ffae586bc2c29dd1667146f8063c151458
breakthatbass <gamache.taylor@gmail.com>
2021-03-02 17:46:57 -0500

    add minish header file

---
+++ src/minish.h
@@ -0,0 +1,14 @@
+#ifndef __MINISH_H__
+#define __MINISH_H__
+
+// shell_exec: execute a command in a child process
+void shell_exec(char **args);
+
+// make_proc: determine if a process goes to stdout or takes in data from stdin
+void make_proc(int in, int out, char **cmd);
+
+// pipe_exec: loop through each command, connecting each through a pipe
+void pipe_exec(char *line);
+
+#endif
+
