bf6df7aad20761448a19d3b786f39b42d9144e81
breakthatbass <gamache.taylor@gmail.com>
2021-03-23 23:08:21 -0400

    assert calloc, clean up mem leaks

--- src/redirect.c
+++ src/redirect.c
@@ -1,6 +1,7 @@
 #include "parse.h"
 #include <unistd.h>
 #include <sys/wait.h>
+#include <assert.h>
 
 #include "redirect.h"
 
@@ -21,12 +22,17 @@ void redirect_in(char **tokens)
 	*(file_arr + 1) = calloc(strlen(file)+1, sizeof(char));
 	*(file_arr + 2) = NULL;
 
+	assert(*file_arr);
+	assert(*(file_arr+1));
+
 	strcpy(*file_arr, "cat");
 	strcpy(*(file_arr+1), file);
 	
 	if (pipe(pfd) < 0) {
 		perror("pipe");
+		exit(EXIT_FAILURE);
 	}
+
 
 	pid1 = fork();
 	if (pid1 < 0) {
@@ -59,6 +65,9 @@ void redirect_in(char **tokens)
 	int status;
 	waitpid(pid1, &status, WUNTRACED);
 	waitpid(pid2, &status, WUNTRACED);
+
+	free(*file_arr);
+	free(*(file_arr+1));
 
 	free(cmd);
 	free(file_arr);
