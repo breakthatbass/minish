4d70f65f9845c6303c7a2ac97b2b8e76dd868696
breakthatbass <gamache.taylor@gmail.com>
2021-03-31 13:18:23 -0400

    fix typo

--- tests/redirect_tests.c
+++ tests/redirect_tests.c
@@ -1,6 +1,7 @@
-#include "minunit/minunit.h"
+//#include "minunit/minunit.h"
 #include "../src/minish.h"
 #include "../src/parse.h"
+#include "../src/redirect.c"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -8,6 +9,7 @@
 int main() 
 {
 	static char *args[20] = {"ls  ", "file", NULL};
-    int a = redirect_right(args);
+    int a = redirect_out(args);
+	if (a == 0) printf("passed!\n");
     return 0;
 }
