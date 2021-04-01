c74f0c47611d005ad3491948d33595d21bf2c1b1
breakthatbass <gamache.taylor@gmail.com>
2021-04-01 14:59:27 -0400

    get tests passing

--- tests/pipe_exec_tests.c
+++ tests/pipe_exec_tests.c
@@ -4,27 +4,57 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static char *args1[20] = {"ls", "wc"};
-static char *args2[20] = {"ls -a", "wc -l"};
+#define MAXSTR 50
+
+static char line2[MAXSTR] = "ls | wc\n";
+static char line1[MAXSTR] = "ls -a | wc -l\n";
+static char line3[MAXSTR] = "ls|wc\n";
+static char line4[MAXSTR] = "ls     |	wc\t\r\n";
+static char line5[MAXSTR] = "ls | wc | wc -l\n";
+
+static char **args1;
+static char **args2;
+static char **args3;
+static char **args4;
+static char **args5;
 
 static int a = 0;
 static int b = 0;
+static int c = 0;
+static int d = 0;
+static int e = 0;
 
 void test_setup(void)
 {   
-    a = pipe_exec(args1);
+	args1 = split(line1, "|");
+	args2 = split(line2, "|");
+	args3 = split(line3, "|");
+	args4 = split(line4, "|");
+	args5 = split(line5, "|");
+    
+	a = pipe_exec(args1);
     b = pipe_exec(args2);
+	c = pipe_exec(args3);
+	d = pipe_exec(args4);
+	e = pipe_exec(args5);
 }
 
 void test_teardown(void)
 {
-    // nothing
+    free(args1);
+	free(args2);
+	free(args3);
+	free(args4);
+	free(args5);
 }
 
 MU_TEST(test_check) 
 {
     mu_check(a == EXIT_SUCCESS);
     mu_check(b == EXIT_SUCCESS);
+    mu_check(c == EXIT_SUCCESS);
+    mu_check(d == EXIT_SUCCESS);
+    mu_check(e == EXIT_SUCCESS);
 }
 
 MU_TEST_SUITE(test_suite)
